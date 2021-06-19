5510aa1fe04f2857ab3383f6da1f64d1d6c9988c
Evan Thacker <25277597+evanthacker@users.noreply.github.com>
2021-06-19 08:29:34 -0400

    Todo Prop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>
+  return (
+  <div>
     <h1>My Todos</h1>
-    <div className='card'>
-      <h2>TITLE</h2>
-      <div className='actions'>
-      <button className='btn'>Delete</button>
-      </div>
-    </div>
-  </div>;
+    <Todo />
+    <Todo />
+    <Todo />
+    <Todo />
+  </div>
+  );
 }
 
 export default App;
