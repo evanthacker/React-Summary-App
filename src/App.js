9b4c3b78578f3fc39b7bc703bf1bd7fbe80fc053
Evan Thacker <25277597+evanthacker@users.noreply.github.com>
2021-06-20 00:11:57 -0400

    Modal & Backdrop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
     <Todo text='Learn Angular'/>
     <Todo text='Learn Vue'/>
     <Todo text='Look At Courses'/>
+    <Modal />
+    <Backdrop />
   </div>
   );
 }
